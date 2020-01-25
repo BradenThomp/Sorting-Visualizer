4367056013c5033d1695c0deb8958b0df7e08044
unknown <bradent@outlook.com>
2020-01-25 12:24:26 -0700

    added colors to selection sort

--- DataPoint.py
+++ DataPoint.py
@@ -2,14 +2,20 @@ from random import randint
 from Colors import *
 import pygame
 
+
 # singular data bar drawn on screen
 class DataPoint:
 
-    def __init__(self, min, max):
+    def __init__(self, min, max, color=WHITE):
         self.height = randint(min, max)
+        self.color = color
 
     # i is the index data lies in an array
     # draws data to the screen
     def draw(self, screen, i):
         pygame.draw.rect(screen, BLACK, (44 + i * 13, 600, 13, -self.height))
-        pygame.draw.rect(screen, WHITE, (45 + i * 13, 600, 12, -self.height))
+        pygame.draw.rect(screen, self.color, (45 + i * 13, 600, 12, -self.height))
+
+    def set_color(self, color):
+        self.color = color
+
--- SelectionSorter.py
+++ SelectionSorter.py
@@ -1,4 +1,5 @@
 from Sorter import Sorter
+from Colors import *
 
 
 class SelectionSorter(Sorter):
@@ -11,10 +12,17 @@ class SelectionSorter(Sorter):
 
             min_index = i
             for j in range(i+1, length):
+                data.my_list[j].set_color(RED)
+                self.force_update()
                 if data.my_list[min_index].height > data.my_list[j].height:
+                    data.my_list[min_index].set_color(WHITE)
                     min_index = j
+                    data.my_list[min_index].set_color(GREEN)
+                else:
+                    data.my_list[j].set_color(WHITE)
 
             data.my_list[i], data.my_list[min_index] = data.my_list[min_index], data.my_list[i]
+            data.my_list[i].set_color(WHITE)
             self.force_update()
 
 
